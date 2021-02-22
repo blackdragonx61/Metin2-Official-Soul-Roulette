41da688108e9cc52fecbb7651ab2a727f3f71ef0
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2021-02-22 20:02:10 +0300

    Update db.log.roulette(run this query).sql

--- 2.Server/db.log.roulette(run this query).sql
+++ 2.Server/db.log.roulette(run this query).sql
@@ -1,7 +1,7 @@
 
 DROP TABLE IF EXISTS `soul_roulette_log`;
 CREATE TABLE `soul_roulette_log`  (
-  `name` varchar(16) CHARACTER SET big5 COLLATE big5_chinese_ci NOT NULL DEFAULT '',
+  `name` varchar(24) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
   `vnum` int(11) UNSIGNED NOT NULL DEFAULT 0,
   `count` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
   `state` enum('OK','ERROR') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'OK',
